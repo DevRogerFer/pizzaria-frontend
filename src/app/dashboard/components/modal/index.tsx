c12e4ec1b5cdde0783316fab197f955f4ba90945
Rogério Fernandes <roferrock@gmail.com>
2025-09-28 01:25:34 -0300

    Finalizing the web project

--- src/app/dashboard/components/modal/index.tsx
+++ src/app/dashboard/components/modal/index.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.scss'
 import { X } from 'lucide-react'
 import { use } from 'react'
 import { OrderContext } from '@/providers/order'
+import { calculateTotalOrder } from '@/lib/helper' 
 
 export function ModalOrder() {
     const { onRequestClose, order, finishOrder } = use(OrderContext);
@@ -32,10 +33,14 @@ export function ModalOrder() {
 
                     {order.map(item => (
                         <section className={styles.item} key={item.id}>
-                            <span><b>( {item.amount} ) - {item.product.name}</b></span>
+                            <span>
+                                <b>QTD: {item.amount} - {item.product.name}</b> - R$ {item.amount * parseFloat(item.product.price)}
+                            </span>
                             <span className={styles.description}>{item.product.description}</span>
                         </section>
                     ))}
+
+                    <h3 className={styles.total}>Valor total: R$ {calculateTotalOrder(order)}</h3>
 
                     <button className={styles.buttonOrder} onClick={handleFinishOrder}>
                         Concluir Pedido
